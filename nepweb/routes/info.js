d1aeeebcbd9115b5882d9d0a74498d37af2a5e3d
Stuart Williams <stuartw@magenic.com>
2018-04-27 06:21:00 -0700

    Working Validation

--- nepweb/routes/info.js
+++ nepweb/routes/info.js
@@ -1,23 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-// const { check, validationResult } = require('express-validator/check')
-// const { matchedData } = require('express-validator/filter')
+const { check, validationResult } = require('express-validator/check')
+const { matchedData } = require('express-validator/filter')
 
 router.get('/', function(req, res, next) {
   res.render('infoForm', { title: 'Info' });
 });
 
 router.post('/frm', 
+[
+  check('email')
+    .isEmail()
+    .withMessage('That email doesn‘t look right')
+    .trim()
+    .normalizeEmail(),
+  check('password')
+    .isLength({ min: 7 })
+    .withMessage('Password must be 7 or more characters')
+    .trim()
+  ], 
   function(req, res, next) {
-    // const errors = validationResult(req)
+    const errors = validationResult(req);
+    var errorMap = errors.mapped();
+    var errorArray = unWindMessages(errorMap);
+
     var info = { };
     info.email = req.body.email;
     info.password = req.body.password;
     info.rememberme = req.body.rememberme;
-    // ,  errors: errors.mapped()
-    res.render('info', { title: 'Info Received', data: info });
+    
+    res.render('info', { title: 'Info Received', data: info,  errors: errorArray });
   }
 );
+
+function unWindMessages(em) {
+  var m = [ ];
+
+  for(var i in em) {
+    m.push(em[i].msg);
+  }
+
+  return m;
+}
+
+
 
 module.exports = router;
